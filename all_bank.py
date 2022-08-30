ee7fefd03e73b53e1488e65ce8965d4433cc74d4
SONEUNBI <treach639@gmail.com>
2022-08-30 13:11:34 +0900

    서울 행정구역 표시 && 지도 위 마커 표시

--- all_bank.py
+++ all_bank.py
@@ -2,7 +2,7 @@ import warnings
 from logging import warning
 import pandas as pd
 import numpy as np
-import csv
+import csv, time
 import pyproj
 from pandas.io.json import json_normalize
 import os
@@ -13,9 +13,18 @@ print(folium.__version__)
 import re
 import googlemaps
 import webbrowser
+import plotly.express as px
 
-my_key = "AIzaSyAPA6edshMP0QnrmGKdyHeWhdbGv9id4yM"
-maps = googlemaps.Client(key=my_key)  # my_key값 입력
+df = pd.read_csv('2021_ALL_BNK_STOR_BASE_change.csv', encoding='UTF-8',usecols=['가로위치좌표', '세로위치좌표'])
+
+df['가로위치좌표'] = pd.to_numeric(df['가로위치좌표'], errors="coerce")
+df['세로위치좌표'] = pd.to_numeric(df['세로위치좌표'], errors="coerce")
+
+
+# my_key = "AIzaSyAPA6edshMP0QnrmGKdyHeWhdbGv9id4yM"
+# maps = googlemaps.Client(key=my_key)  # my_key값 입력
+
+
 
 # with open('2021_ALL_BNK_STOR_BASE_change.csv', encoding='cp949',usecols=['가로위치좌표','세로위치좌표']) as csv_file:
 #     csv_data = csv.reader(csv_file)
@@ -25,60 +34,60 @@ maps = googlemaps.Client(key=my_key)  # my_key값 입력
 #             print(d, end ='\t')
 #         print()
 
-df = pd.read_csv('2021_ALL_BNK_STOR_BASE_change.csv', encoding='UTF-8',usecols=['가로위치좌표', '세로위치좌표'])
+# df = pd.read_csv('2021_ALL_BNK_STOR_BASE_change.csv', encoding='UTF-8',usecols=['가로위치좌표', '세로위치좌표'])
 
-df['가로위치좌표'] = pd.to_numeric(df['가로위치좌표'], errors="coerce")
-df['세로위치좌표'] = pd.to_numeric(df['세로위치좌표'], errors="coerce")
+# df['가로위치좌표'] = pd.to_numeric(df['가로위치좌표'], errors="coerce")
+# df['세로위치좌표'] = pd.to_numeric(df['세로위치좌표'], errors="coerce")
 
-df = df.dropna()
-df.index=range(len(df))
-df.tail()
+# df = df.dropna()
+# df.index=range(len(df))
+# df.tail()
 
-# 좌표계 변환 함수
-def project_array(coord, p1_type, p2_type):
-    """
-    좌표계 변환 함수
-    - coord: x, y 좌표 정보가 담긴 NumPy Array
-    - p1_type: 입력 좌표계 정보 ex) epsg:5179
-    - p2_type: 출력 좌표계 정보 ex) epsg:4326
-    """
-    p1 = pyproj.Proj(init=p1_type)
-    p2 = pyproj.Proj(init=p2_type)
-    fx, fy = pyproj.transform(p1, p2, coord[:, 0], coord[:, 1])
-    return np.dstack([fx, fy])[0]
+# # 좌표계 변환 함수
+# def project_array(coord, p1_type, p2_type):
+#     """
+#     좌표계 변환 함수
+#     - coord: x, y 좌표 정보가 담긴 NumPy Array
+#     - p1_type: 입력 좌표계 정보 ex) epsg:5179
+#     - p2_type: 출력 좌표계 정보 ex) epsg:4326
+#     """
+#     p1 = pyproj.Proj(init=p1_type)
+#     p2 = pyproj.Proj(init=p2_type)
+#     fx, fy = pyproj.transform(p1, p2, coord[:, 0], coord[:, 1])
+#     return np.dstack([fx, fy])[0]
 
-coord = np.array(df)
-coord
+# coord = np.array(df)
+# coord
 
-# 좌표계 정보 설정
-p1_type = "epsg:2097"
-p2_type = "epsg:4326"
+# # 좌표계 정보 설정
+# p1_type = "epsg:2097"
+# p2_type = "epsg:4326"
 
-# project_array() 함수 실행
-result = project_array(coord, p1_type, p2_type)
-result
+# # project_array() 함수 실행
+# result = project_array(coord, p1_type, p2_type)
+# result
 
-print(result)
+# print(result)
 
-df['경도'] = result[:, 0]
-df['위도'] = result[:, 1]
+# df['경도'] = result[:, 0]
+# df['위도'] = result[:, 1]
 
-df.tail()
+# df.tail()
 
-# 데이터 100개 랜덤 추출
-sample = df.sample(n=100)
+# # 데이터 100개 랜덤 추출
+# sample = df.sample(n=100)
 
-# 지도 중심 좌표 설정
-lat_c, lon_c = 37.53165351203043, 126.9974246490573
+# # 지도 중심 좌표 설정
+# lat_c, lon_c = 37.53165351203043, 126.9974246490573
 
-# Folium 지도 객체 생성
-m = folium.Map(location=[lat_c, lon_c], zoom_start=6, tiles = 'cartodbpositron')
+# # Folium 지도 객체 생성
+# m = folium.Map(location=[lat_c, lon_c], zoom_start=6, tiles = 'cartodbpositron')
 
-geo_json ='https://raw.githubusercontent.com/southkorea/seoul-maps/master/kostat/2013/json/seoul_municipalities_geo_simple.json'
-# 마커 생성
-for _, row in sample.iterrows():
-    lat, lon = row['위도'], row['경도']
-    folium.Marker(location=[lat, lon]).add_to(m)
+# geo_json ='https://raw.githubusercontent.com/southkorea/seoul-maps/master/kostat/2013/json/seoul_municipalities_geo_simple.json'
+# # 마커 생성
+# for _, row in sample.iterrows():
+#     lat, lon = row['위도'], row['경도']
+#     folium.Marker(location=[lat, lon]).add_to(m)
 
 # folium.Choropleth(
 #     geo_data = geo_json,
@@ -92,4 +101,4 @@ for _, row in sample.iterrows():
 #     legend_name = 'Population (people)'
 # ).add_to(m)
 
-m
+# m
