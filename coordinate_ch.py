579f78da1e541c8eeeabe3447a3313b23c5800b0
SONEUNBI <treach639@gmail.com>
2022-08-31 22:32:10 +0900

    행정구역별 표시

--- coordinate_ch.py
+++ coordinate_ch.py
@@ -3,6 +3,6 @@ import numpy as np
 import pyproj
 import folium
 
-df = pd.read_csv("./sample/fulldata_07_24_04_P_일반음식점.csv",
+df = pd.read_csv("./2021_ALL_BNK_STOR_BASE_change.csv",
                  encoding='cp949',
                  usecols=['좌표정보(x)','좌표정보(y)'])
