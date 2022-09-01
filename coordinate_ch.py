dda48b554c8030abbca7c46d87f97d2c1047c6bf
SONEUNBI <treach639@gmail.com>
2022-09-01 10:24:44 +0900

    은행별 & 지역별 추출

--- coordinate_ch.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import pyproj
-import folium
-
-df = pd.read_csv("./2021_ALL_BNK_STOR_BASE_change.csv",
-                 encoding='cp949',
-                 usecols=['좌표정보(x)','좌표정보(y)'])
